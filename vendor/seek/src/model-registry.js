a940b91c189bc2afe1fcd68ef7ab1e7ac98bc4ae
VegaForce02 <videultravegapunk+30@proton.me>
2026-09-12 05:57:49 +0000

    Add runtime model registry shim

---
+++ vendor/seek/src/model-registry.js
@@ -0,0 +1,14 @@
+export const ACTIVE_MODEL_SPEC = {
+    key: 'tooape/granite-embedding-97m-multilingual-r2-GBQ4-ONNX',
+    repo: 'tooape/granite-embedding-97m-multilingual-r2-GBQ4-ONNX',
+    revision: '54db88c5667bd79b4aea24ea6027a7ef45a7bbb5',
+    files: [
+        'config.json',
+        'tokenizer.json',
+        'tokenizer_config.json',
+        'special_tokens_map.json',
+        'onnx/model_q4.onnx',
+    ],
+    dim: 384,
+    dtype: 'q4',
+};
